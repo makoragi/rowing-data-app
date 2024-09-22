d871d98790ff1cb663839361c6cc861f9e4b0dc2
Makoto Sakuragi <makoragi@gmail.com>
2024-09-22 12:50:52 +0900

    speedの目盛り変更

--- src/utils/formatters.js
+++ src/utils/formatters.js
@@ -1,11 +1,15 @@
 export const formatYAxis = (value, dataKey) => {
-  // 整数に丸めて表示
+  if (dataKey === 'speed') {
+    return value.toFixed(1);
+  }
   return Math.round(value).toString();
 };
 
 export const formatTooltip = (value, name) => {
+  if (name === 'speed') {
+    return value.toFixed(2);
+  }
   if (typeof value === 'number') {
-    // ツールチップでは小数点以下2桁まで表示
     return value.toFixed(2);
   }
   return value;
