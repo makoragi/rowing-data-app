cf2d2faefaa358982ade0a33cadbdb35c1b5a7c2
Makoto Sakuragi <makoragi@gmail.com>
2024-09-22 19:04:37 +0900

    strokeRate-distancePerStroke 追加

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,6 +4,7 @@ export const graphOptions = [
   { value: 'distance-strokeRate', label: 'Distance vs Stroke Rate', y1: 'distance', y2: 'strokeRate', unit1: 'm', unit2: 'SPM' },
   { value: 'speed-strokeRate', label: 'Speed vs Stroke Rate', y1: 'speed', y2: 'strokeRate', unit1: 'm/s', unit2: 'SPM' },
   { value: 'speed-distancePerStroke', label: 'Speed vs Distance/Stroke', y1: 'speed', y2: 'distancePerStroke', unit1: 'm/s', unit2: 'm' },
+  { value: 'strokeRate-distancePerStroke', label: 'Stroke Rate vs Distance/Stroke', y1: 'strokeRate', y2: 'distancePerStroke', unit1: 'SPM', unit2: 'm' },
   // { value: 'distance-elapsedTime', label: 'Distance vs Elapsed Time', y1: 'distance', y2: 'elapsedTime', unit1: 'm', unit2: '' },
   // { value: 'speed-elapsedTime', label: 'Speed vs Elapsed Time', y1: 'speed', y2: 'elapsedTime', unit1: 'm/s', unit2: '' },
 ];
