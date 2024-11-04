f506e0fd54bb053481c05b5d940865e8615f2d38
makoragi <makoragi@gmail.com>
2024-11-04 23:23:08 +0900

    区間ごとに数字

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -1,8 +1,18 @@
 import React from 'react';
-import { MapContainer, TileLayer, Polyline } from 'react-leaflet';
+import { MapContainer, TileLayer, Polyline, Marker, Tooltip } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
 
-// 色のリスト（区間ごとに使用）
+// カスタムアイコンを作成
+const createIcon = (label) => {
+  return L.divIcon({
+    className: 'custom-div-icon',
+    html: `<div style="background-color:#2e8b57; color:white; border-radius: 50%; padding: 5px;">${label}</div>`,
+    iconSize: [30, 30],
+    iconAnchor: [15, 15],
+  });
+};
+
 const COLORS = ['blue', 'green', 'red', 'orange', 'purple', 'cyan'];
 
 const MapView = ({ segments }) => {
@@ -12,20 +22,56 @@ const MapView = ({ segments }) => {
 
   // 最初の位置（地図の初期表示に使用）
   const position = [segments[0][0].gpsLat, segments[0][0].gpsLon];
-  
+
   return (
     <MapContainer center={position} zoom={13} style={{ height: '400px', width: '100%' }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
+
+      {/* セグメントの描画 */}
       {segments.map((segment, index) => (
         <Polyline
           key={index}
           positions={segment.map(coord => [coord.gpsLat, coord.gpsLon])}
-          color={COLORS[index % COLORS.length]} // 色を区間ごとに割り当て
+          color={COLORS[index % COLORS.length]}
         />
       ))}
+
+      {/* スタート地点のマーカー */}
+      <Marker
+        position={[segments[0][0].gpsLat, segments[0][0].gpsLon]}
+        icon={createIcon('S')}
+      >
+        <Tooltip direction="top" offset={[0, -10]} opacity={1}>
+          スタート地点
+        </Tooltip>
+      </Marker>
+
+      {/* 各1000m終了地点のマーカー */}
+      {segments.map((segment, index) => (
+        <Marker
+          key={`segment-end-${index}`}
+          position={[segment[segment.length - 1].gpsLat, segment[segment.length - 1].gpsLon]}
+          icon={createIcon((index + 1).toString())}
+        >
+          <Tooltip direction="top" offset={[0, -10]} opacity={1}>
+            {`${(index + 1) * 1000}m地点`}
+          </Tooltip>
+        </Marker>
+      ))}
+
+      {/* ゴール地点のマーカー */}
+      <Marker
+        position={[segments[segments.length - 1][segments[segments.length - 1].length - 1].gpsLat,
+                   segments[segments.length - 1][segments[segments.length - 1].length - 1].gpsLon]}
+        icon={createIcon('G')}
+      >
+        <Tooltip direction="top" offset={[0, -10]} opacity={1}>
+          ゴール地点
+        </Tooltip>
+      </Marker>
     </MapContainer>
   );
 };
